e7780740d42c327aeadc10d434c0f381a277603d
Blagoje <93238316+bagip@users.noreply.github.com>
2022-05-26 14:02:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+import numpy as np
+import os
+import pyautogui as pg
+from playsound import playsound
+
+def won_round():
+    if won == None:
+        print(won)
+    else:
+        playsound("C:/blagoje/opencv project/res/Bruh.wav")
+
+def lost_round():
+    if lost == None:
+        print(lost)
+    else:
+        playsound("C:/blagoje/opencv project/res/wow.wav")
+
+
+def team_ace():
+    if teamace == None:
+        print(teamace)
+    else:
+        playsound("C:/blagoje/opencv project/res/wow.wav")
+        
+
+while True:
+    won = pg.locateOnScreen("C:/blagoje/opencv project/res/won3.png",grayscale = True, confidence = 0.65)
+    lost = pg.locateOnScreen("C:/blagoje/opencv project/res/lost2.png",grayscale = True, confidence = 0.65)
+    teamace = pg.locateOnScreen("C:/blagoje/opencv project/res/TeamAce.png",grayscale = True, confidence = 0.65)
+    won_round()
+    lost_round()
+    team_ace()
+
+
+
+
+
+
+    
