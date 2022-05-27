977db476c2c1c30b8399a502813d015507d61d5c
Blagoje <93238316+bagip@users.noreply.github.com>
2022-05-27 14:50:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from playsound import playsound
 
 
 path = os.path.dirname(__file__)
+won_image = path + "/res/won2.png"
+lost_image = path + "/res/lost2.png"
 
 
 def won_round():
@@ -22,10 +24,11 @@ def lost_round():
 
 
 while True:
-    won = pg.locateOnScreen(path + "/res/won3.png",grayscale = True, confidence = 0.65, region=(600,120,300,200))
-    lost = pg.locateOnScreen(path +"/res/lost2.png",grayscale = True, confidence = 0.65, region=(600,120,300,200))
+    won = pg.locateOnScreen(won_image,grayscale = True, confidence = 0.65, region=(700,155,300,200))
+    lost = pg.locateOnScreen(lost_image,grayscale = True, confidence = 0.65, region=(700,155,300,200))
     won_round()
     lost_round()
+    
 
 
 
